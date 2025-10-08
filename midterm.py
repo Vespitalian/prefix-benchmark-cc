f57ce5b8cdf1345c639ccbd605850aaac2e02780
Vespitalian <=>
2025-10-07 21:03:42 -0700

    Reorganized files, started working on creating the graph

--- midterm.py
+++ midterm.py
@@ -1,54 +1,11 @@
-import time
-import numpy as np
 import pandas as pd
-from english import SIXES
+import matplotlib.pyplot as plt
 
-# The slow
-prefixes = ['c','ca','car','care','cares']
+df = pd.read_csv('benchmark_results.csv')
 
-def valid_prefix_slow(prefix, word_list): # My code
-    for i in range(len(prefix)):
-        for word in word_list:
-            if word[:i+1] == prefix:
-                return True
-    return False
+pre_labels = ['c','ca','car','care','cares']
+columns = ['slow','medium','fast']
 
-
-# The medium
-def valid_prefix_med(prefix, word_list): # The improvement that I came up with for my code
-    for word in word_list:
-        if prefix == word[:len(prefix)]:
-            return True
-    return False
-
-# The fast
-def valid_prefix_fast(prefix, word_list): # The Calvin solution
-    return any(word.startswith(prefix) for word in word_list)
-
-results = []
-for prefix in prefixes:
-    start_slow = time.time() # Getting time before we call function
-    for i in range(10000): # Repeating the function 10,000 times to have a bigger number :)
-        valid_prefix_slow(prefix,SIXES)
-    end_slow = time.time() # End timer
-    slow_time = end_slow - start_slow # The difference between start and the end time
-
-    start_med = time.time()
-    for i in range(10000):
-        valid_prefix_med(prefix, SIXES)
-    end_med = time.time()
-    med_time = end_med - start_med
-
-    start_fast = time.time()
-    for i in range(10000):
-        valid_prefix_fast(prefix, SIXES)
-    end_fast = time.time()
-    fast_time = end_fast - start_fast
-    
-    results += [[slow_time, med_time, fast_time]]
-
-df = pd.DataFrame(
-    data=np.array(results)
-)
-
-print(df)
+plt.ylabel('Time in seconds over 10,000 runs')
+plt.title('Prefix Benchmark Comparison')
+plt.savefig('prefix_benchmark.png')
